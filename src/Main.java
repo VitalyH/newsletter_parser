50880b28596155dfe38a8725fe778ca188ddb846
Vitaly Hlapkovsky <vitaly.hlapkovsky@gmail.com>
2018-07-25 11:02:03 +0300

    working prototype

--- src/Main.java
+++ src/Main.java
@@ -12,18 +12,32 @@ public class Main {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(INPUT_FILE)));
              BufferedWriter writer = new BufferedWriter(new FileWriter(OUTPUT_FILE))) {
 
-            String line;
+            String line;        // Line of text with markers
+            String cleanLine;   // Line of text w/o markers
+            String div = "</div>";
+
             while ((line = br.readLine()) != null) {
-                writer.write(line);
+
+                // Split the string
+                String tempArray[] = line.split("=");
+
+                // Clean the string from the marker
+                cleanLine = tempArray[1];
+
+                // Check if line has needed marker
+                // Add the code accordingly
+                switch (tempArray[0]) {
+                    case "b": cleanLine = "BBB " + cleanLine + div; break;
+                    case "c": cleanLine = "CCC " + cleanLine + div; break;
+                }
+                // Write result into the output file
+                writer.write(cleanLine);
                 writer.newLine();
 
-                //todo For testing, DELETE later
-                System.out.println(line);
             }
 
         } catch (IOException e) {
             e.printStackTrace();
-
         }
     }
 }
